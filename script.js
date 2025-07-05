52b0981cfaff5baa265108ce94dbf813fbf3caf2
Pasindu115530 <pasinduudana12m2@gmail.com>
2025-07-06 03:04:04 +0530

    Page 98% done.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
 let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav a');
 
@@ -18,4 +27,30 @@ window.onscroll = () => {
             }
         }
     });
+
+
+let header = document.querySelector('header');
+header.classList.toggle('sticky', window.scrollY > 100);
+
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
 };
+
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration:2000,
+    delay: 200 
+
+});
+ScrollReveal().reveal('.home .content , .heading',  { origin: 'top' });
+ScrollReveal().reveal('.home-img , .projects-container, .contact form , .education-row ',  { origin: 'bottom' });
+ScrollReveal().reveal('.home .content p , .about-content',  { origin: 'left' });
+
+const typed = new Typed('.speedtext',{
+    strings: ['Project Manager', 'Project Manager', 'Project Manager'],
+    typespeed: 300,
+    backspeed: 300,
+    backDelay: 1000,
+    loop:true
+});
